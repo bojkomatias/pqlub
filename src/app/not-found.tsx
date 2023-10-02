f212883c33ce05b700215d874fc2e54f300f9dba
Nicolás Horn <nicoskate000@gmail.com>
2023-10-02 19:28:25 -0300

    Changed everything!!!

---
+++ src/app/not-found.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/no-unescaped-entities */
+import Link from 'next/link'
+
+import { Container } from '@/components/Container'
+import { FadeIn } from '@/components/FadeIn'
+
+export default function NotFound() {
+  return (
+    <Container className="mb-72 flex h-full items-center pt-24 sm:pt-32 lg:pt-40">
+      <FadeIn className="flex max-w-xl flex-col items-center text-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-6 w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
+          />
+        </svg>
+
+        <h1 className="mt-4 font-display text-2xl font-semibold text-neutral-950">
+          Page is under construction
+        </h1>
+        <p className="mt-2 text-sm text-neutral-600">
+          Sorry, we're still building this page!
+        </p>
+        <Link
+          href="/"
+          className="mt-4 text-sm font-semibold text-neutral-950 transition hover:text-neutral-700"
+        >
+          Go to the home page
+        </Link>
+      </FadeIn>
+    </Container>
+  )
+}
